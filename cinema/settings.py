d98445645933ee08cee9e0232383ea03e50eb3eb
Valery Filatov <valiklfiatoff@yandex.ru>
2024-03-14 12:17:34 +0300

    Remove pyodbc

--- cinema/settings.py
+++ cinema/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cinema',
         'USER': 'admin',
         'PASSWORD': 'admin',
